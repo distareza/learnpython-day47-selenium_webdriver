4f73039145b714946257b2a7799a01994352e3d9
Dista Reza <dista.reza@bertelsmann.de>
2022-05-20 07:21:22 +0800

    update

--- my_configuration.py
+++ my_configuration.py
@@ -6,3 +6,5 @@ linkedin_user = config.get("linkedin.com", "linkedin_user")
 linkedin_pwd = config.get("linkedin.com", "linkedin_pwd")
 my_contact_phone_no = config.get("my-personal-info", "phone_no")
 
+my_facebook_account = config.get("facebook.com", "fb_user")
+my_facebook_password = config.get("facebook.com", "fb_pwd")
