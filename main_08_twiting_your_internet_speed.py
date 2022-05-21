0a57272abe1cd664c99574e6e6b042a91444e77e
Dista Reza <dista.reza@bertelsmann.de>
2022-05-21 09:11:04 +0800

    update

--- main_08_twiting_your_internet_speed.py
+++ main_08_twiting_your_internet_speed.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException, NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 import my_configuration
@@ -16,8 +17,7 @@ TWITTER_PASSWORD = my_configuration.my_twitter_password
 
 class InternetSpeedTwitterBot:
     def __init__(self, driver_path):
-        selenium_service = Service(executable_path=driver_path)
-        self.driver = webdriver.Chrome(service=selenium_service)
+        self.selenium_service = Service(executable_path=driver_path)
         self.up = 0
         self.down = 0
 
@@ -25,77 +25,110 @@ class InternetSpeedTwitterBot:
 
         timeout = 5
         print("opening speed test")
-        self.driver.get("https://www.speedtest.net/")
+        caps = DesiredCapabilities().CHROME
+        # caps["pageLoadStrategy"] = "normal"  #  Waits for full page load
+        caps["pageLoadStrategy"] = "none"  # Do not wait for full page load
+        driver = webdriver.Chrome(service=self.selenium_service, desired_capabilities=caps)
+        driver.get("https://www.speedtest.net/")
+
+        while True:
+            try:
+                # find go button
+                driver.find_element(by=By.CSS_SELECTOR, value=".start-button a")
+                break
+            except NoSuchElementException:
+                print('...no start button yet')
+                try :
+                    WebDriverWait(driver, timeout=10).until(
+                        EC.visibility_of_element_located((By.CSS_SELECTOR, ".start-button a")))
+                except TimeoutException:
+                    sleep(1)
+
         print('speed test is loaded')
 
         # Depending on your location, you might need to accept the GDPR pop-up.
         # accept_button = self.driver.find_element_by_id("_evidon-banner-acceptbutton")
         # accept_button.click()
 
-        go_button = self.driver.find_element(by=By.CSS_SELECTOR, value=".start-button a")
+        go_button = driver.find_element(by=By.CSS_SELECTOR, value=".start-button a")
         go_button.click()
-        print('click go botton')
-        sleep(60)
-        self.up = self.driver.find_element(by=By.XPATH, value=
-            '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
-        self.down = self.driver.find_element(by=By.XPATH, value=
-            '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span').text
+        print('click go button')
+
+        # sleep(60)
+        xpath_up = '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span'
+        xpath_down = '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span'
+        xpath_feedback = '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[3]/div/div/div[2]/div/div/h3'
+
+        # wait until feed back element is available
+        while True:
+            try:
+                driver.find_element(by=By.XPATH, value=xpath_feedback)
+                break
+            except NoSuchElementException:
+                print("...no feedback element yet")
+                try:
+                    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, xpath_feedback)))
+                except TimeoutException:
+                    sleep(1)
+
+        self.down = float(driver.find_element(by=By.XPATH, value=xpath_down).text)
+        self.up = float(driver.find_element(by=By.XPATH, value=xpath_up).text)
 
         print(f'Get Up speed : {self.up}')
         print(f'Get Down speed : {self.down}')
 
-    def tweet_at_provider(self):
-        self.driver.get("https://twitter.com/login")
+    def tweet_my_speed_connection(self):
+        print("open twitter.com")
+        caps = DesiredCapabilities().CHROME
+        caps["pageLoadStrategy"] = "normal"  # Waits for full page load
+        driver = webdriver.Chrome(service=self.selenium_service, desired_capabilities=caps)
+        driver.get("https://twitter.com/login")
 
         sleep(2)
-        # email = self.driver.find_element(by=By.XPATH, value=
-        #      '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[1]/label/div/div[2]/div/input')
-        # password = self.driver.find_element(by=By.XPATH, value=
-        #      '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[2]/label/div/div[2]/div/input')
+        xpath_twitter_email_input = '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input'
+        xpath_twitter_account_input = '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/label/div/div[2]/div/input'
+        xpath_twitter_password_input = '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input'
+        xpath_twitter_next_button = '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div/div'
+        xpath_twitter_compose_input = '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/label/div[1]/div/div/div/div/div[2]/div/div/div/div'
+        xpath_twitter_tweet_button = '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[3]/div/div/div[2]/div[3]/div/span/span'
 
-        email = self.driver.find_element(by=By.XPATH, value=
-           '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input')
-        next_button = self.driver.find_element(by=By.XPATH, value=
-            '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]')
-
+        print("Enter email in twitter")
+        email = driver.find_element(by=By.XPATH, value=xpath_twitter_email_input)
         email.send_keys(TWITTER_EMAIL)
-        next_button.click()
+        email.send_keys(Keys.ENTER)
+
         sleep(1)
 
-        account = self.driver.find_element(by=By.XPATH, value=
-            '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/label/div/div[2]/div/input')
+        print("enter twitter account")
+        account = driver.find_element(by=By.XPATH, value=xpath_twitter_account_input)
         account.send_keys(TWITTER_ACCOUNT)
-        next_button = self.driver.find_element(by=By.XPATH, value=
-            '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div/div')
+        next_button = driver.find_element(by=By.XPATH, value=xpath_twitter_next_button)
         next_button.click()
         sleep(1)
 
-        password = self.driver.find_element(by=By.XPATH, value=
-           '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input')
+        print("enter password")
+        password = driver.find_element(by=By.XPATH, value=xpath_twitter_password_input)
         password.send_keys(TWITTER_PASSWORD)
         sleep(2)
+
+        print("click login")
         password.send_keys(Keys.ENTER)
 
         sleep(5)
-        # tweet_compose = self.driver.find_element(by=By.XPATH, value=
-        #     '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div[2]/div/div/div/div')
-        tweet_compose = self.driver.find_element(by=By.XPATH, value=
-            '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/label/div[1]/div/div/div/div/div[2]/div/div/div/div')
-
+        print("enter twitter message")
+        tweet_compose = driver.find_element(by=By.XPATH, value=xpath_twitter_compose_input)
         tweet = f"Test, my Internet speed {self.down}down/{self.up}up "
         tweet_compose.send_keys(tweet)
         sleep(3)
 
-        # tweet_button = self.driver.find_element(by=By.XPATH, value=
-        #     '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[4]/div/div/div[2]/div[3]')
-        tweet_button = self.driver.find_element(by=By.XPATH, value=
-            '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[3]/div/div/div[2]/div/div')
+        print("submit message")
+        tweet_button = driver.find_element(by=By.XPATH, value=xpath_twitter_tweet_button)
         tweet_button.click()
 
         sleep(2)
-        self.driver.quit()
+        driver.quit()
+
 
 bot = InternetSpeedTwitterBot(CHROME_DRIVER_PATH)
 bot.get_internet_speed()
-bot.tweet_at_provider()
-
+bot.tweet_my_speed_connection()
